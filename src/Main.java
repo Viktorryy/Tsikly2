8a2527c4dc4a43efaa95f4d802dad8e238468100
Viktorryy <viktory_o@rambler.ru>
2022-09-26 21:19:29 +0400

    // Домашнее задание 3         // задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         // Домашнее задание 3
-        // задача 1
+        // задача 2
 
-        int yearNow = 2022;
-        int period = 79;
-        int year0 = 0;
-        int year200 = yearNow - 200;
-        int year100 = yearNow + 100;
+        int factor = 2;
+        int product = 0;
 
-        while (year0 <= year100 ) {
-            year0 = year0 + period;
-            if (year0 >= year200 && year0 <= year100) {
-                System.out.println(year0);
-
-            }
+       for (int i = 1; i <= 10; i++) {
+            product = factor * i;
+            System.out.println(factor + " * " + i + " = " + product);
 
         }
 
