1f6627afc8bc6c7f9b53a3f94219fbb5b84af4a0
Viktorryy <viktory_o@rambler.ru>
2022-09-26 18:52:03 +0400

    // Домашнее задание 2         // задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         // Домашнее задание 2
-        // задача 1
+        // задача 2
 
-        int total = 0;
-        int vklad = 15000;
-        int monthVklad = 0;
 
-        while (total<2459000) {
-            total = vklad + total;
-            monthVklad = monthVklad + 1;
-            System.out.println("Месяц " + monthVklad +", сумма накоплений равна " +
-                    total+ " рублей");
+        int i = 1;
 
+        while ( i <= 10) {
+          System.out.print(" " + i);
+          i = i + 1;
         }
+        System.out.println();
 
+        for (i = 10; i >= 1; i-- ) {
+            System.out.print(" " + i);
+        }
     }
 }
