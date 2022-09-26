9c2682b1d85d4a3c7c38244e2e91c3d9b3dc83aa
Viktorryy <viktory_o@rambler.ru>
2022-09-26 20:23:52 +0400

    // Домашнее задание 1         // задача 2
           вопрос: Как вывести цифры с копейками?
           не в формате с плавающей точкой  Е в степени

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Домашнее задание 1
-        // задача 1
+        // задача 2
 
 
         int vklad = 15000;
@@ -12,8 +12,11 @@ public class Main {
         while (total <= 12_000_000) {
             monthVklad = monthVklad + 1;
             total = vklad + total + total / 100 * percentYear / 12;
+            if (monthVklad % 6 == 0) {
+                System.out.println("Месяц  " + monthVklad +
+                        ", сумма накоплений составляет " + total + " рублей");
+            }
 
-            System.out.println("Месяц  " + monthVklad + ", сумма накоплений составляет " + total);
         }
 
 
