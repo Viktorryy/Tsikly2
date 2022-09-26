6a39d4156552349122c0591764d549defd2c4708
Viktorryy <viktory_o@rambler.ru>
2022-09-26 21:11:57 +0400

    // Домашнее задание 3         // задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        // Домашнее задание 1
-        // задача 4
+        // Домашнее задание 3
+        // задача 1
 
+        int yearNow = 2022;
+        int period = 79;
+        int year0 = 0;
+        int year200 = yearNow - 200;
+        int year100 = yearNow + 100;
 
-        int dayFriday = 1;
-        int dayAll = 31;
+        while (year0 <= year100 ) {
+            year0 = year0 + period;
+            if (year0 >= year200 && year0 <= year100) {
+                System.out.println(year0);
 
-        while (dayFriday <= dayAll ) {
-
-            System.out.println("Сегодня пятница," + dayFriday + "-е число. Необходимо подготовить отчет.");
-            dayFriday = dayFriday + 7;
+            }
 
         }
 
