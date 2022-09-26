7109f43a1ecbf68a242bd8054c1a2fa7e6c1abbf
Viktorryy <viktory_o@rambler.ru>
2022-09-26 18:45:43 +0400

    // Домашнее задание 2         // задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        // Домашнее задание 2
+        // задача 1
 
+        int total = 0;
+        int vklad = 15000;
+        int monthVklad = 0;
 
+        while (total<2459000) {
+            total = vklad + total;
+            monthVklad = monthVklad + 1;
+            System.out.println("Месяц " + monthVklad +", сумма накоплений равна " +
+                    total+ " рублей");
 
-        System.out.println("Hello world!");
+        }
+
     }
 }
