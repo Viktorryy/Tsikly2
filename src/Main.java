3ba0aaee6547788c0ce822ec0b1706544c8a3dd2
Viktorryy <viktory_o@rambler.ru>
2022-09-26 20:51:23 +0400

    // Домашнее задание 1         // задача 4

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         // Домашнее задание 1
-        // задача 2
+        // задача 4
 
 
-        int vklad = 15000;
-        int percentYear = 7;
-        int monthVklad = 0;
-        int monthAll = 108;
-        int total = 0;
+        int dayFriday = 1;
+        int dayAll = 31;
 
-        for (;monthVklad <= monthAll;monthVklad = monthVklad + 1) {
+        while (dayFriday <= dayAll ) {
 
-            total = vklad + total + total / 100 * percentYear / 12;
-            if (monthVklad % 6 == 0) {
-                System.out.println("Месяц  " + monthVklad +
-                        ", сумма накоплений составляет " + total + " рублей");
-            }
+            System.out.println("Сегодня пятница," + dayFriday + "-е число. Необходимо подготовить отчет.");
+            dayFriday = dayFriday + 7;
 
         }
-        System.out.println("За 9 лет, сумма накоплений составит " + total + " рублей");
+
 
     }
 }
